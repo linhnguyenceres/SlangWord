bd42f22d0c2ceba68ecd41d2c303849bc8492023
linhnguyenceres <simpsonelena35@gmail.com>
2021-04-17 22:23:41 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+        public static void main(String[] args) {
+        System.out.print("Tôi yêu lập trình.");
+        System.out.print("Thích Java và uống cafe.");
+    }
+}
