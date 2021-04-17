2d6e6adfcf3e6fbb620a1584923352cc9525b590
linhnguyenceres <simpsonelena35@gmail.com>
2021-04-17 23:25:58 +0700

    create keyWordKey, MeaningKey, SlangKey

--- src/main/java/MeaningKey.java
+++ src/main/java/MeaningKey.java
@@ -1,14 +1,71 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany;
 
 /**
  *
  * @author ASUS
  */
 public class MeaningKey {
+    static ArrayList<String> History = new ArrayList<>();
+    private static HashMap<String, String>SlangWord;
+    static ArrayList<String> fromStringToSlang(List<String>input){
+        
+    }
     
+    static ArrayList<String> fromStringToMeaning(List<String>input){
+        
+    }
+    
+    static HashMap<String, String> meaningKey(List<String>slang, List<String>meaning){
+        
+    }
+    
+    public String findSlangWord(String input){
+        return SlangWord.get(input);
+    }
+    
+    public void inputWord(String in){
+        History.add(in + ": "+SlangWord.get(in));
+    }
+    
+    public boolean existSlang(String in){
+        return SlangWord.containsKey(in);
+    }
+    
+    public void addNewWord(String slangin, String meaning in){
+        
+    }
+    
+    public void deleteWord(String in){
+        
+    }
+    
+    public void editWord(String oldSlang, String newSlang, String newMeaning){
+        
+    }
+    
+    
+    public void reset(){
+        
+    }
+    
+    public void quiz(){
+        
+    }
+    
+    public void setSlangWordWithMeaningKey(List<String>input){
+        
+    }
+    
+    public List<String> getSlangWord(){
+        
+    }
 }
--- src/main/java/SlangKey.java
+++ src/main/java/SlangKey.java
@@ -1,14 +1,50 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany;
 
 /**
  *
  * @author ASUS
  */
 public class SlangKey {
+    static ArrayList<String> History = new ArrayList<>();
+    private static HashMap<String, String>SlangWord;
+    static ArrayList<String> fromStringToSlang(List<String>input){
+        
+    }
     
+    static ArrayList<String> fromStringToMeaning(List<String>input){
+        
+    }
+    
+    static HashMap<String, String> slangKey(List<String>slang, List<String>meaning){
+        HashMap<String, String> temp = new HashMap<>();
+        for(int i=0; i< slang.size; i++){
+            
+        }
+        return temp;
+    }
+    
+    public String findSlangWord(String input){
+        return SlangWord.get(input);
+    }
+    
+    public void inputWord(String in){
+        History.add(in + ": " + SlangWord.get(in));
+    }
+    
+    public void showHistory(){
+        
+    }
+    
+    static boolean existSlang(String in){
+        
+    }
 }
